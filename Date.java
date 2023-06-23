39d6d833d6993915e4ba72db547cf57f9818c444
Francisco <franciscocmfreitas@gmail.com>
2023-06-22 21:07:38 -0300

    coisas simples no date simples

--- Date.java
+++ Date.java
@@ -3,7 +3,7 @@ import java.time.LocalDate;
  * Mostra a data em que a compra foi feita e consequentemente a data do voo. 
  * @param buyDate usa o Metodo LocalDate, para pegar o dia (sem horario) em que o voo parte. Porem no formato da lingua inglesa(YYYY/MM/DD).
  * foi necessario importar o pacote java.time e usar o a funcao .now() que ultiliza como base o computador em que o programa esta sendo rodado.
- * @param date String que recebe dataDaCompra, para permitir que o formato passe para o portugues.
+ * @param date String que recebe buyDate, para permitir que o formato passe para o portugues.
  * @param day String que recebe, de acordo com o charAt de date, o dia da compra e do voo.
  * @param month String que recebe, de acordo com o charAt de date, o mes da compra e do voo.
  * @param year String que recebe, de acordo com o charAt de date, o ano da compra e do voo.
